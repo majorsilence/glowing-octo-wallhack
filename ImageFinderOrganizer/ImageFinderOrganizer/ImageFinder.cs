7d07e2856351f42570151b9b6c7c22d45ff0f95f
Peter Gill <peter@majorsilence.com>
2015-01-18 16:35:36 -0330

    Find the image taken date and copy to destination folder.

--- ImageFinderOrganizer/ImageFinderOrganizer/ImageFinder.cs
+++ ImageFinderOrganizer/ImageFinderOrganizer/ImageFinder.cs
@@ -10,34 +10,72 @@ using System.Linq;
 using System.Xml.Linq;
 using System.Threading.Tasks;
 using System.IO;
+using System.Text.RegularExpressions;
+using System.Drawing.Imaging;
+using System.Text;
 namespace ImageFinderOrganizer
 {
-	public class ImageFinder
-	{
+    public class ImageFinder
+    {
 
-        
+
         internal Task Execute(DirectoryInfo searchBase, DirectoryInfo destBase)
         {
 
-            return Task.Run(() => CopyFilesRecursively(searchBase, destBase));
+            return Task.Run(() => FindAndCopyFiles(searchBase, destBase));
 
         }
 
+        // *********************
+        // See http://stackoverflow.com/questions/180030/how-can-i-find-out-when-a-picture-was-actually-taken-in-c-sharp-running-on-vista 
+        //we init this once so that if the function is repeatedly called
+        //it isn't stressing the garbage man
+        private static Regex r = new Regex(":");
 
-        private void CopyFilesRecursively(DirectoryInfo source, DirectoryInfo target)
+        //retrieves the datetime WITHOUT loading the whole image
+        private static DateTime GetDateTakenFromImage(string path)
         {
-            foreach (DirectoryInfo dir in source.GetDirectories())
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            using (Image myImage = Image.FromStream(fs, false, false))
             {
-                CopyFilesRecursively(dir, target.CreateSubdirectory(dir.Name));
+                PropertyItem propItem = myImage.GetPropertyItem(36867);
+                string dateTaken = r.Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2);
+                return DateTime.Parse(dateTaken);
             }
-            foreach (FileInfo file in source.GetFiles())
-            {
-                
-                file.CopyTo(Path.Combine(target.FullName, file.Name), true); //overwrite
-            }
-
-          
         }
 
-	}
+        // *********************
+
+        private void FindAndCopyFiles(DirectoryInfo source, DirectoryInfo target)
+        {
+
+            var files = Directory.EnumerateFiles(source.FullName, "*.*", SearchOption.AllDirectories)
+            .Where(s => s.EndsWith(".JPG") || s.EndsWith(".jpg") || s.EndsWith(".JPEG") || s.EndsWith(".jpeg"));
+
+            foreach (string file in files)
+            {
+                DateTime takenDate = GetDateTakenFromImage(file);
+                CopyFile(file, target.FullName, takenDate.Year);
+            }
+
+        }
+
+        private void CopyFile(string srcFile, string targetFolder, int yearTaken)
+        {
+            string filename = System.IO.Path.GetFileNameWithoutExtension(srcFile) + ".jpg";
+            string destFile = System.IO.Path.Combine(targetFolder, yearTaken.ToString(), filename);
+
+            int count = 1;
+            while (System.IO.File.Exists(destFile))
+            {
+                filename = System.IO.Path.GetFileNameWithoutExtension(srcFile) 
+                    + string.Format("_{1}.jpg", count);
+                destFile = System.IO.Path.Combine(targetFolder, yearTaken.ToString(), filename);
+                count++;
+            }
+            System.IO.File.Copy(srcFile, destFile, false);
+
+        }
+
+    }
 }
